9904facf8e5be490f863fd0283a4bb6919de870b
autumn-ragland <raglanda998@gmail.com>
2019-01-11 15:39:19 -0600

    CW1 and CW2 added

---
+++ CW1.js
@@ -0,0 +1,12 @@
+//Using the code below: printNumbers();
+// Create a printNumbers function to print integers from -25 to 20.
+
+function printNumbers()
+{
+for(i=-25; i<=20; i++)
+{
+    console.log(i)
+}
+}
+
+printNumbers();
---
+++ CW2.js
@@ -0,0 +1,8 @@
+//Create a function that’s passed a firstName and lastName. Return lastName comma firstName.
+
+function name(x,y)
+{
+    return y + "," + x
+}
+
+console.log(name("autumn", "ragland"));
